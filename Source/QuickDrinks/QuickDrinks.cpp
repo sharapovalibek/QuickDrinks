0e77cb47acbfc46b11a7778c30196d1c6240afc5
alibek <createforwork@gmail.com>
2025-06-28 22:35:22 +0600

    initial unreal files

---
+++ Source/QuickDrinks/QuickDrinks.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QuickDrinks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuickDrinks, "QuickDrinks" );
